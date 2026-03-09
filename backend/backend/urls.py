f5bc9b60830423dcd10ee6a670d8957227c9528d
Ilkhomjon Rakhmatov <emailingiz@gmail.com>
2026-03-09 18:15:01 +0500

    Add templates and views for home and about pages; update settings and URLs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from online_shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
--- backend/online_shop/views.py
+++ backend/online_shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    text = "Hello world"
+    return render(request, "home.html", {"text": text})
+
+
+def about(request):
+    return render(request, "about.html")    
